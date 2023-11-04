e89d65f8bf56faae558cb8fa6b8ec91a7717fdc8
okumuratks <okmrtks517170@gmailcom>
2023-11-04 09:53:50 +0900

    gitコマンド確認テスト

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
+//githubテスト用プログラム
 class Hello{
   public static void main(String[] args){
-    System.out.println("Hello");
+    int num = 0;
+  while (num < 5) {
+    num += 1;
+     System.out.println("Hello");
   }
-}
